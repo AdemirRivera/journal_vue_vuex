c742032596c7690af5b48baba4e7f14d92f499af
AdemirRivera <54855416+AdemirRivera@users.noreply.github.com>
2022-02-17 17:28:52 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,26 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), 
+            date: new Date().toDateString(),
+            text: 'Nisi pariatur sint eiusmod esse deserunt velit commodo tempor magna laborum elit elit adipisicing eiusmod. Sit occaecat labore ullamco mollit eiusmod non reprehenderit nostrud nostrud tempor veniam enim ex velit. Cillum Lorem cillum officia sint reprehenderit. Mollit duis amet duis dolore consequat est ex reprehenderit proident duis occaecat. Minim reprehenderit do deserunt sunt minim pariatur elit sit id id sint. Nostrud aute nulla fugiat sint.',
+            picture: null,
+
+        },
+        {
+            id: new Date().getTime() +  1, 
+            date: new Date().toDateString(),
+            text: 'Sint dolor sit Lorem ut ipsum id eiusmod minim voluptate anim. Nostrud consectetur sunt adipisicing reprehenderit incididunt cillum esse nostrud. Mollit non Lorem laborum culpa velit irure sunt laborum laboris ad enim. Ullamco laboris exercitation veniam adipisicing est Lorem occaecat cillum fugiat cupidatat do.',
+            picture: null,
+
+        },
+        {
+            id: new Date().getTime() + 2, 
+            date: new Date().toDateString(),
+            text: 'Duis duis sit pariatur reprehenderit laboris. In deserunt excepteur incididunt excepteur eiusmod mollit adipisicing aliqua enim. Reprehenderit qui et occaecat ipsum cupidatat ea quis mollit proident aute eu tempor esse. Aute sit fugiat nostrud sint. In ut pariatur minim velit est labore ipsum ad qui laboris Lorem. Officia culpa nisi do dolor officia magna laboris ad.',
+            picture: null,
+
+        }
+    ]
+})
